8d90706ab5366729a8b8107b12858c007bcd1320
Paromita Banik <banikparomita111@gmail.com>
2019-05-19 03:54:17 +0200

    connecting heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends Component {
     }
 
     onSubmitSignIn = () => {
-    	fetch('http://localhost:3000/register', {
+    	fetch('https://murmuring-brook-35488.herokuapp.com/register', {
     		method: 'post',
     		headers: {'Content-Type':'application/json'},
     		body: JSON.stringify({
